af3dff06581f72f0629b38c0eaff0504cf766eb2
zizy97 <supuntharuka9749@gmail.com>
2021-11-05 00:17:40 +0530

    added toplogo finalized the navbar

--- src/components/TopLogoBar.js
+++ src/components/TopLogoBar.js
@@ -1,59 +1,145 @@
-import React from "react";
+import React, { useContext } from "react";
 import styled from "styled-components";
+import { ThemeContext } from "../contexts/ThemeContext";
 import { logo } from "./CommonComponents";
 
 import { Container, Navbar } from "./CommonComponents";
 
 const IconImage = styled.img`
-  height: 50px;
-  width: 40px;
   margin-left: 4.5px;
   cursor: pointer;
+
+  @media only screen and (min-width: 1160px) {
+    height: 50px;
+    width: 40px;
+  }
+
+  @media (min-width: 1024px) and (max-width: 1160px) {
+    height: 45px;
+    width: 35px;
+  }
+
+  @media (min-width: 700px) and (max-width: 1024px) {
+    height: 40px;
+    width: 30px;
+    margin-left: 5px;
+  }
+  @media (max-width: 700px) {
+    height: 40px;
+    width: 30px;
+    margin-left: 9px;
+  }
 `;
 
 const TopTitle = styled.h1`
   position: absolute;
-  font-size: 30px;
-  color: white;
-  font-family: "Lora", serif;
-  letter-spacing: 8px;
-  padding-left: 30px;
-  margin-top: -20px;
-  margin-bottom: 1px;
-`;
-
-const CustomDiv = styled.div`
   @media only screen and (min-width: 1160px) {
-    background: red;
+    font-size: 30px;
+    letter-spacing: 8px;
+    padding-left: 30px;
+    margin-top: -20px;
+    margin-bottom: 1px;
   }
 
   @media (min-width: 1024px) and (max-width: 1160px) {
-    background: blue;
+    font-size: 25px;
+    letter-spacing: 8px;
+    padding-left: 20px;
+    margin-top: -15px;
+    margin-bottom: 1px;
   }
 
   @media (min-width: 700px) and (max-width: 1024px) {
-    background: pink;
+    font-size: 16px;
+    letter-spacing: 4.5px;
+    padding-left: 20px;
+    margin-top: -10px;
+    margin-bottom: 1px;
   }
   @media (min-width: 500px) and (max-width: 700px) {
-    background: purple;
+    font-size: 16px;
+    letter-spacing: 2px;
+    padding-left: 20px;
+    margin-top: -10px;
+    margin-bottom: 1px;
   }
   @media (min-width: 350px) and (max-width: 500px) {
-    background: gray;
+    font-size: 13px;
+    letter-spacing: 0px;
+    padding-left: 12px;
+    margin-top: -10px;
+    margin-bottom: 1px;
   }
   @media (max-width: 350px) {
-    background: black;
+    font-size: 9px;
+    letter-spacing: 1px;
+    padding-left: 8px;
+    margin-top: -10px;
+    margin-bottom: 1px;
+  }
+`;
+
+const CustomDiv = styled.div`
+  background: ${({ bg }) => bg};
+  font-family: ${({ fonts }) => fonts.topLogo};
+  color: white;
+`;
+
+const AnimatedText = styled.span`
+  font-weight: 700;
+  text-align: center;
+  font-family: Hack, sans-serif;
+  text-transform: uppercase;
+  background: linear-gradient(30deg, #000, #fff, #000);
+  letter-spacing: 5px;
+  -webkit-background-clip: text;
+  background-clip: text;
+  -webkit-text-fill-color: transparent;
+  background-repeat: no-repeat;
+  background-size: 80%;
+  animation: shine 10s linear infinite;
+  position: relative;
+  @keyframes shine {
+    0% {
+      background-position-x: -500%;
+    }
+    25% {
+      background-position-x: -250%;
+    }
+    50% {
+      background-position-x: 0%;
+    }
+    75% {
+      background-position-x: 250%;
+    }
+    100% {
+      background-position-x: 500%;
+    }
+  }
+  @media only screen and (min-width: 1160px) {
+    font-size: 15px;
+  }
+
+  @media (min-width: 1024px) and (max-width: 1160px) {
+    font-size: 12px;
+  }
+
+  @media (min-width: 700px) and (max-width: 1024px) {
+    font-size: 10px;
+  }
+  @media (max-width: 700px) {
+    font-size: 0px;
+    visibility: hidden;
   }
 `;
 
 export default function TopLogoBar() {
+  const { theme, light, dark, fonts } = useContext(ThemeContext);
+  const them = theme ? light : dark;
+
   return (
-    <CustomDiv>
-      <Navbar
-        sticky="top"
-        variant="dark"
-        expand="md"
-        className="navstyle"
-      >
+    <CustomDiv bg={them.ui} fonts={fonts}>
+      <Navbar sticky="top" variant="dark" expand="md" className="navstyle">
         <Container fluid>
           <div>
             <table>
@@ -61,7 +147,9 @@ export default function TopLogoBar() {
                 <tr>
                   <IconImage alt="" src={logo} />
                   <th>
-                    <TopTitle className="text">Industrial Placement Office (IPO)</TopTitle>
+                    <TopTitle className="text">
+                      Industrial Placement Office (IPO)
+                    </TopTitle>
                   </th>
                 </tr>
                 <tr>
@@ -72,10 +160,14 @@ export default function TopLogoBar() {
             </table>
           </div>
           <div>
-            {/* <MovingTexts>
-                <p className="text1">Faculty Of Agriculture</p>
-                <p className="text2">University Of Ruhuna</p>
-                </MovingTexts> */}
+            <AnimatedText>
+              <span>Faculty Of Agriculture</span>
+            {/* </AnimatedText>
+         
+            <AnimatedText> */}
+               <br />
+              <span>University Of Ruhuna</span>
+            </AnimatedText>
           </div>
         </Container>
       </Navbar>
--- src/contexts/ThemeContext.js
+++ src/contexts/ThemeContext.js
@@ -7,7 +7,7 @@ function ThemeContextProvider(props) {
 
   const light = { syntax: "#000", ui: "linear-gradient(to right, #000428, #004e92)", bg: "#eee", color: "red", footer:{syntax:"#000",f_ic_bg:"yellow", f_c_bg:"green", copyright:"#333"} };//footer_icon_bg,footer_content_bg
   const dark = { syntax: "#000", ui: "linear-gradient(to right, #000428, #004e92)", bg: "#555", color: "blue", footer:{syntax:"#fff",f_ic_bg:"#212529", f_c_bg:"#000", copyright:"#ffffff99"} };
-  const fonts = { general:"'Quicksand',sans-serif",address:"time new roman" };
+  const fonts = { general:"'Quicksand',sans-serif",address:"time new roman",topLogo:'"Lora", serif' };
 
 
   const toggleTheme = () => {
