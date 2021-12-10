b74f39fdd35b7f72898a8d4f8b9ccea71c4c4463
Nadun Nethsara <nadunnethsara456@gmail.com>
2021-12-10 23:56:46 +0530

    Added contact us and subscription

--- src/containers/HomeContent.js
+++ src/containers/HomeContent.js
@@ -2,6 +2,9 @@ import React from 'react'
 import ImageSlider from '../components/ImageSlider'
 import Mover from '../components/Mover'
 import NewsList from '../containers/NewsList'
+import ContactUs from '../components/ContactUs'
+import Subscription from '../components/Subscription'
+import { Container,Row,Col } from '../components/CommonComponents'
 
 export default function HomeContent() {
     return (
@@ -9,6 +12,12 @@ export default function HomeContent() {
             <ImageSlider />
             <Mover/>
             <NewsList/>
+            <Container fluid>
+                <Row>
+                    <ContactUs/>
+                    <Subscription/>
+                </Row>
+            </Container>
         </>
     )
 }
