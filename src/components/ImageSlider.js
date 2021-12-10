f4170862c4f29dcacf60af5b743abdc5edf8cff5
buddhika-b <buddhikahalangoda@gmail.com>
2021-12-10 10:56:20 +0530

    img slider modified

--- src/components/ImageSlider.js
+++ src/components/ImageSlider.js
@@ -106,46 +106,9 @@ const AnimatedText = styled.span`
 function ImageSlider() {
   const { theme, light, dark } = useContext(ThemeContext);
 
-<<<<<<< HEAD
-  const current_theme = theme ? light : dark;
-  console.log(current_theme);
-
-  return (
-    <Himage>
-      <Carousel indicators="" slide="" touch="true" controls="">
-        <Carousel.Item>
-          <ImageTag
-            src="https://images.unsplash.com/photo-1594686948539-2709857f7066?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80"
-            alt="Second slide"
-          />
-        </Carousel.Item>
-        <Carousel.Item>
-          <ImageTag
-            src="https://images.unsplash.com/photo-1541339907198-e08756dedf3f?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80"
-            alt="Third slide"
-          />
-        </Carousel.Item>
-        <Carousel.Item>
-          <ImageTag
-            src="https://images.unsplash.com/photo-1506377872008-6645d9d29ef7?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80"
-            alt="Third slide"
-          />
-        </Carousel.Item>
-        <Carousel.Item>
-          <ImageTag
-            src="https://images.unsplash.com/photo-1523240795612-9a054b0db644?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80"
-            alt="Third slide"
-          />
-        </Carousel.Item>
-      </Carousel>
-      <AnimatedText>
-        <span>Welcome to IPO</span>
-        {/* </AnimatedText>
-=======
     const current_theme = theme ? light : dark;
  
     return(
->>>>>>> 6fe3cf18a7bb05d59ea1737a0ea4ad5fa00938ba
       
         <Himage>
         <Carousel indicators="" slide="" touch="true" controls="">
