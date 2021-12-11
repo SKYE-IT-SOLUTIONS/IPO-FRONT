f841d09825615c179fa9f067782e117633468324
zizy97 <supuntharuka9749@gmail.com>
2021-12-11 13:41:25 +0530

    update

--- src/contexts/ThemeContext.js
+++ src/contexts/ThemeContext.js
@@ -6,7 +6,7 @@ function ThemeContextProvider(props) {
   const [isLightTheme, setisLightTheme] = useState(false);
 
   const light = { syntax: "#000", ui: "linear-gradient(to right, #000428, #004e92)", bg: "#eee", color: "red", footer:{syntax:"#000",f_ic_bg:"yellow", f_c_bg:"green", copyright:"#333"},button:{enable:"red",disable:"grey"} };//footer_icon_bg,footer_content_bg
-  const dark = { syntax: "#000", ui: "linear-gradient(to right, #000428, #004e92)", bg: "#555", color: "blue", footer:{syntax:"#fff",f_ic_bg:"#212529", f_c_bg:"rgba(0,0,0,0.2)", copyright:"#ffffff99"},button:{enable:"red",disable:"grey"} };
+  const dark = { syntax: "#000", ui: "linear-gradient(to right, #000428, #004e92)", bg: "#555", color: "blue", footer:{syntax:"#fff",f_ic_bg:"#212529", f_c_bg:"rgba(0,0,0,0.2)", copyright:"#ffffff99"},button:{enable:"green",disable:"grey"} };
   const fonts = { general:"'Quicksand',sans-serif",address:"time new roman",topLogo:'"Lora", serif',title:"'Righteous', cursive" };
 
 
--- src/routes.js
+++ src/routes.js
@@ -6,18 +6,11 @@ import AddJobPost from './components/AddJobPost';
 import PostJob from './components/PostJob'
 import NewsView from './components/NewsView';
 import AddNewsPost from './components/AddNewsPost';
-import { isUser } from './api/auth/authAPI';
 
-const auth = async () => {
-  const {status, error} = await isUser();
-  console.log(error);
-  return status;
-};
-
-const routes = () => [
+const routes = (isAuthenticated) => [
     {
       path: '/admin',
-      element: auth() ? <h1>UserLogged</h1> : <Navigate to='/login' />,
+      element: isAuthenticated ? <h1>UserLogged{console.log("isAuthenticated "+ isAuthenticated)}</h1> : <Navigate to='/login' />,
       children: [
         { path: 'dashboard/ongoing', element: "" },
         { path: '*', element: <Navigate to='/404' /> }
@@ -40,7 +33,7 @@ const routes = () => [
       path: '/ass',
       element: <h1>Hello</h1>,
       children: [
-        { path: 'login', element: auth() ? <Navigate to='/' /> : <Navigate to='/404' /> },
+        { path: 'login', element: isAuthenticated ? <Navigate to='/' /> : <Navigate to='/404' /> },
         { path: 'register', element:"" },
         { path: 'delivery', element: "" },
       ]
