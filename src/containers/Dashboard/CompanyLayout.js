3efc233cc10799693dc1f05572b531ba13481ab3
zizy97 <supuntharuka9749@gmail.com>
2021-12-19 16:35:31 +0530

    code fixing

--- src/containers/Dashboard/CompanyLayout.js
+++ src/containers/Dashboard/CompanyLayout.js
@@ -5,7 +5,7 @@ import { Outlet } from "react-router-dom";
 import { Header, Main, Footer } from "../../components/DashboardLayout";
 
 import TopLogoBar from "../../components/TopLogoBar";
-import NavBar from "../../components/NavBar";
+// import NavBar from "../../components/NavBar";
 import FooterContent from "../../components/Footer";
 import CompanySideNav from "./Company/CompanySideNav";
 
