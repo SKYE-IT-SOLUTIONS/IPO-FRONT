0aa8ec0ccfda81371a7280304a4ec0c67be4a354
Nadun Nethsara <nadunnethsara456@gmail.com>
2022-01-06 01:22:03 +0530

    Update Admin News and Job

--- src/components/SelectNews.js
+++ src/components/SelectNews.js
@@ -1,98 +1,29 @@
-import React , {useContext} from 'react';
-import { styled } from '@mui/material/styles';
-import Box from '@mui/material/Box';
-import ButtonBase from '@mui/material/ButtonBase';
-import Typography from '@mui/material/Typography';
-import addnews from '../assets/addnews.svg'
-import newsList from '../assets/newsList.svg'
+import React,{useContext} from "react";
+import { Container, Row, Col ,CustomButton } from "./CommonComponents";
+import styled from "styled-components";
+import { Icon } from "@iconify/react";
+import news from "../assets/News-pana.svg";
 import { ThemeContext } from "../contexts/ThemeContext";
-import {Container} from "./CommonComponents";
 import { useNavigate } from 'react-router-dom';
 
-  const  OuterDiv = styled(Container)`
-    font-family: ${({ font }) => font.general};
-    text-align: center;
-  `
+const NavButton = styled(CustomButton)`
+  padding: 5px 15px;
+  width: 300px;
+  border-radius: 25px;
+  border: 2px solid rgba(0, 0, 0, 1);
+  background-color: rgba(234, 97, 141, 0.05);
+  margin: 5px 0;
 
-const images = [
-  {
-    url: newsList,
-    title: 'View News List',
-    width: '50%',
-    nav:"list"
-  },
-  {
-    url: addnews,
-    title: 'Add News',
-    width: '50%',
-    nav:"addNews"
-  },
-];
+  @media (min-width: 768px) and (max-width: 1040px) {
+    width: 250px;
+    font-size: 14px;
+  }
 
-const ImageButton = styled(ButtonBase)(({ theme }) => ({
-  position: 'relative',
-  height: 400,
-  [theme.breakpoints.down('sm')]: {
-    width: '100% !important', // Overrides inline-style
-    height: 100,
-  },
-  '&:hover, &.Mui-focusVisible': {
-    zIndex: 1,
-    '& .MuiImageBackdrop-root': {
-      opacity: 0.15,
-    },
-    '& .MuiImageMarked-root': {
-      opacity: 0,
-    },
-    '& .MuiTypography-root': {
-      border: '4px solid white',
-    },
-  },
-}));
-
-const ImageSrc = styled('span')({
-  position: 'absolute',
-  left: 0,
-  right: 0,
-  top: 0,
-  bottom: 0,
-  backgroundSize: 'cover',
-  backgroundPosition: 'center 40%',
-});
-
-const Image = styled('span')(({ theme }) => ({
-  position: 'absolute',
-  left: 0,
-  right: 0,
-  top: 0,
-  bottom: 0,
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'center',
-  color: theme.palette.common.white,
-  
-}));
-
-const ImageBackdrop = styled('span')(({ theme }) => ({
-  position: 'absolute',
-  left: 0,
-  right: 0,
-  top: 0,
-  bottom: 0,
-  backgroundColor: theme.palette.common.black,
-  opacity: 0.4,
-  transition: theme.transitions.create('opacity'),
-}));
-
-const ImageMarked = styled('span')(({ theme }) => ({
-  height: 3,
-  width: 18,
-  backgroundColor: theme.palette.common.white,
-  position: 'absolute',
-  bottom: -2,
-  left: 'calc(50% - 9px)',
-  transition: theme.transitions.create('opacity'),
-}));
+  @media (max-width: 768px) {
+    width: 250px;
+    font-size: 14px;
+  }
+`;
 
 const AnimatedText = styled('h1')`
   font-family: ${({ font }) => font.general};
@@ -102,44 +33,74 @@ const AnimatedText = styled('h1')`
   padding: 25px 0;
 `;
 
-export default function ButtonBases() {
-    const { fonts } = useContext(ThemeContext);
-    const navigate = useNavigate();
+const OuterDiv = styled(Container)`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  min-height: 76vh;
+  font-family: ${({ font }) => font.general};
+`;
+
+const ApplyImage = styled.div`
+  height: 450px;
+  width: 100%;
+  background-image: url(${({ image }) => image});
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+  margin: auto;
+
+  @media (min-width: 768px) and (max-width: 1040px) {
+    height: 400px;
+  width: 100%;
+  }
+
+  @media (max-width: 768px) {
+    height: 350px;
+  width: 100%;
+  }
+`;
+
+const Column = styled(Col)`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+ 
+`
+
+const InnerDiv = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  text-align: center;
+  padding-bottom: 30px;
+`
+
+function SelectNews() {
+  const { fonts } = useContext(ThemeContext);
+  const navigate = useNavigate();
 
   return (
-      <OuterDiv font={fonts}>
-          <AnimatedText font={fonts}>Choose You Choice</AnimatedText>
-        <Box sx={{ display: 'flex', flexWrap: 'wrap', minWidth: 300, width: '100%'}}>
-      {images.map((image) => (
-        <ImageButton
-          focusRipple
-          key={image.title}
-          style={{
-            width: image.width,
-          }}
-          onClick={()=>{navigate(image.nav)}}
-        >
-          <ImageSrc style={{ backgroundImage: `url(${image.url})`}} />
-          <ImageBackdrop className="MuiImageBackdrop-root"/>
-          <Image>
-            <Typography
-              component="span"
-              variant="h5"
-              style={{color:"white",fontWeight:"bolder"}}
-              sx={{
-                position: 'relative',
-                p: 4,
-                pt: 2,
-                pb: (theme) => `calc(${theme.spacing(1)} + 6px)`,
-              }}
-            >
-              {image.title}
-              <ImageMarked className="MuiImageMarked-root" />
-            </Typography>
-          </Image>
-        </ImageButton>
-      ))}
-    </Box>
-      </OuterDiv>
+    <OuterDiv  font={fonts}>
+      <AnimatedText font={fonts}>Choose You Choice</AnimatedText>
+      <Row>
+        <Column md={5}>
+          <InnerDiv style={{ margin: "auto"}}>
+                  <NavButton onClick={()=>{
+                    navigate("addNews")
+                  }}>Add News</NavButton>
+                  <NavButton onClick={()=>{
+                    navigate("list")
+                  }}>View News List</NavButton>
+                  <NavButton>View Non Approved List</NavButton>
+          </InnerDiv>
+        </Column>
+        <Col md={7}>
+          <ApplyImage image={news} />
+        </Col>
+      </Row>
+    </OuterDiv>
   );
 }
+
+export default SelectNews;
