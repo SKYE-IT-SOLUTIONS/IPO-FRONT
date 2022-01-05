274eaeb42de6db92bf53562dfb6241638d56bcae
zizy97 <supuntharuka9749@gmail.com>
2022-01-06 04:11:28 +0530

    update

--- src/shared/axios.js
+++ src/shared/axios.js
@@ -3,8 +3,8 @@ import axios from "axios";
 const ipoAPIHostProd = "http://uipp.agri.ruh.ac.lk:8080/api"; //for production
 // const ipoAPIHostDev = "http://localhost:3005"; // for development
 // const ipoAPIHostDev = "http://localhost:8081/api"; // for development
-const ipoAPIHostDev = "http://uipp.agri.ruh.ac.lk:8080/api"; // for development
-// const ipoAPIHostDev = "https://ipobackendapi.herokuapp.com/api"; // for development
+// const ipoAPIHostDev = "http://uipp.agri.ruh.ac.lk:8080/api"; // for development
+const ipoAPIHostDev = "https://ipobackendapi.herokuapp.com/api"; // for development
 
 
 // const ipoAPIHost = process.env.NODE_ENV === "development" ?  ipoAPIHostProd:ipoAPIHostDev ;
