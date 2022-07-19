d5e207504aad3129f130ac496264f2132501eea3
zizy97 <supuntharuka9749@gmail.com>
2022-07-20 01:06:02 +0530

    account added

--- src/containers/Dashboard/common/Profile.js
+++ src/containers/Dashboard/common/Profile.js
@@ -1,5 +1,49 @@
-const Profile = () => {
-  return <h1>profile</h1>;
-};
+import { Box, Container, Grid, Typography } from '@mui/material';
+import AccountProfile from '../../../components/account/AccountProfile';
+import ProfileDetails from '../../../components/account/ProfileDetails';
+
+
+const Profile = () => (
+  <>
+    <Box
+      component="main"
+      sx={{
+        flexGrow: 1,
+        py: 2
+      }}
+    >
+      <Container maxWidth="lg">
+        <Typography
+          sx={{ mb: 3 }}
+          variant="h4"
+        >
+          Profile
+        </Typography>
+        <Grid
+          container
+          spacing={3}
+        >
+          <Grid
+            item
+            lg={4}
+            md={6}
+            xs={12}
+          >
+            <AccountProfile />
+          </Grid>
+          <Grid
+            item
+            lg={8}
+            md={6}
+            xs={12}
+          >
+            <ProfileDetails />
+          </Grid>
+        </Grid>
+      </Container>
+    </Box>
+  </>
+);
+
 
 export default Profile;
