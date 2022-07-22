09d01636832c51b71e9dd6c0dfd08ca5cfceb5c8
Sandaruwan Lakshitha <sandaru.laka@gmail.com>
2022-07-23 01:06:33 +0530

    add head icon and change to axios to production

--- src/shared/axios.js
+++ src/shared/axios.js
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 // const ipoAPIHostProd = "http://uipp.agri.ruh.ac.lk:8080/api"; //for production
-const ipoAPIHostProd =
-  "http://uipp.agri.ruh.ac.lk:8080/idustial-0.0.1-SNAPSHOT/api"; //for production
+const ipoAPIHostProd = "https://ipo-agri.herokuapp.com/api"; //for production
 
 // const ipoAPIHostDev = "http://localhost:8081/api"; // for development
 // const ipoAPIHostDev = "http://uipp.agri.ruh.ac.lk:8080/api"; // for development
@@ -10,10 +9,10 @@ const ipoAPIHostDev = "https://ipo-agri.herokuapp.com/api"; // for development
 
 // const ipoAPIHost = process.env.NODE_ENV === "development" ?  ipoAPIHostProd:ipoAPIHostDev ;
 const ipoAPIHost =
-  process.env.NODE_ENV === "development" ? ipoAPIHostDev : ipoAPIHostProd;
+   process.env.NODE_ENV === "development" ? ipoAPIHostDev : ipoAPIHostProd;
 
 const instance = axios.create({
-  baseURL: ipoAPIHost,
+   baseURL: ipoAPIHost,
 });
 
 export default instance;
