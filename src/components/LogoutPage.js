01d4140ca8da87aa4f7ee0c91218ae69fcc6938b
rking32 <sljohnwick@gmail.com>
2022-07-22 15:07:20 +0530

    deploy gh-pages

--- src/components/LogoutPage.js
+++ src/components/LogoutPage.js
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { Spinner } from "react-bootstrap";
-import { Navigate, useNavigate } from "react-router-dom";
+import { Navigate } from "react-router-dom";
 
 import AuthServices from "../services/AuthServices";
 
@@ -8,7 +8,6 @@ const authServices = new AuthServices();
 
 const LogoutPage = () => {
   const [isLoading, setIsLoading] = useState(false);
-  // const navigate = useNavigate();
   useEffect(() => {
     setIsLoading(true);
     authServices.handleLogout().then(() => {
--- src/shared/axios.js
+++ src/shared/axios.js
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 // const ipoAPIHostProd = "http://uipp.agri.ruh.ac.lk:8080/api"; //for production
-const ipoAPIHostProd =
-  "http://uipp.agri.ruh.ac.lk:8080/idustial-0.0.1-SNAPSHOT/api"; //for production
+const ipoAPIHostProd = "https://ipo-agri.herokuapp.com/api"; //for production
 
 // const ipoAPIHostDev = "http://localhost:8081/api"; // for development
 // const ipoAPIHostDev = "http://uipp.agri.ruh.ac.lk:8080/api"; // for development
