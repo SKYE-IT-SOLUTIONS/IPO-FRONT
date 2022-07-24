0a2920e5742f240955feac46121fb65a1f06801f
Sandaruwan Lakshitha <sandaru.laka@gmail.com>
2022-07-25 00:54:50 +0530

    fixed reCapture

--- src/components/common/Captcha.js
+++ src/components/common/Captcha.js
@@ -2,18 +2,21 @@ import React from "react";
 import ReCAPTCHA from "react-google-recaptcha";
 
 function onChange(value) {
-  console.log("Captcha value:", value);
+   console.log("Captcha value:", value);
 }
 
 function Captcha() {
-  return (
-    <div>
-      <ReCAPTCHA
-        sitekey="6LcBFxIhAAAAAFg_OfQq9DZ6e7dqk6V5Oi0SzjCs"
-        onChange={onChange}
-      />
-    </div>
-  );
+   return (
+      <div>
+         {
+            <ReCAPTCHA
+               sitekey="6LdRBhkhAAAAAGku-pnVSat2HZ1ogUVf4rCheUcr"
+               onChange={onChange}
+               onError={onChange}
+            />
+         }
+      </div>
+   );
 }
 
 export default Captcha;
--- src/components/home/Subscription.js
+++ src/components/home/Subscription.js
@@ -1,5 +1,5 @@
 /* eslint-disable no-unused-vars */
-import React, { useContext, useState } from "react";
+import React, { useCallback, useContext, useState } from "react";
 import { Icon } from "@iconify/react";
 import styled from "styled-components";
 import { ThemeContext } from "../../contexts/ThemeContext";
@@ -16,192 +16,194 @@ import { Recaptcha } from "../CommonComponents";
 import { Box } from "@mui/system";
 
 const CssTextField = muistyle(TextField)({
-  margin: "8px auto",
-  "& .MuiOutlinedInput-root": {
-    "& fieldset": {
-      borderColor: "black",
-    },
-  },
+   margin: "8px auto",
+   "& .MuiOutlinedInput-root": {
+      "& fieldset": {
+         borderColor: "black",
+      },
+   },
 });
 
 const Img = styled.img`
-  width: 450px;
-  height: 450px;
+   width: 450px;
+   height: 450px;
 
-  @media (min-width: 1000px) and (max-width: 1263px) {
-    height: 360px;
-    width: 360px;
-  }
+   @media (min-width: 1000px) and (max-width: 1263px) {
+      height: 360px;
+      width: 360px;
+   }
 
-  @media (max-width: 1000px) {
-    display: none;
-  }
+   @media (max-width: 1000px) {
+      display: none;
+   }
 `;
 
 const SubscriptDiv = styled.div`
-  display: flex;
-  flex-direction: column;
-  text-align: center;
-  justify-content: center;
-  font-family: ${({ fonts }) => fonts.general};
-  /* background: linear-gradient(to top, #00b09b, #96c93d); */
+   display: flex;
+   flex-direction: column;
+   text-align: center;
+   justify-content: center;
+   font-family: ${({ fonts }) => fonts.general};
+   /* background: linear-gradient(to top, #00b09b, #96c93d); */
 `;
 
 const InfoLable = styled(Lable)`
-  font-size: 13px;
-  color: black;
-  padding-top: 0px;
-  padding-bottom: 0px;
+   font-size: 13px;
+   color: black;
+   padding-top: 0px;
+   padding-bottom: 0px;
 `;
 
 const SubIcon = styled(Icon)`
-  margin: auto;
-  color: #0f3443;
+   margin: auto;
+   color: #0f3443;
 `;
 
 const SubBttn = styled(CustomButton)`
-  margin: 10px auto;
-  padding: auto;
+   margin: 10px auto;
+   padding: auto;
 `;
 
 const Title = styled.h1`
-  font-family: ${({ fonts }) => fonts.title};
-  font-size: 25px;
-  color: blue;
+   font-family: ${({ fonts }) => fonts.title};
+   font-size: 25px;
+   color: blue;
 `;
 const RECAPTCHA = styled(Recaptcha)`
-  margin: auto;
-  /* display: flex;
-  justify-content: center; */
+   margin: auto;
+   /* display: flex;
+   justify-content: center;
+   transform: translateX(50%); */
 `;
 
 function Subscription() {
-  const { fonts } = useContext(ThemeContext);
+   const { fonts } = useContext(ThemeContext);
 
-  const [name, setName] = useState("");
-  const [nameInfo, setNameInfo] = useState({ error: null, status: false });
+   const [name, setName] = useState("");
+   const [nameInfo, setNameInfo] = useState({ error: null, status: false });
 
-  const [email, setEmail] = useState("");
-  const [emailInfo, setEmailInfo] = useState({ error: null, status: false });
+   const [email, setEmail] = useState("");
+   const [emailInfo, setEmailInfo] = useState({ error: null, status: false });
 
-  const [isLoading, setisLoading] = useState(false);
-  // const [submitError, setSubmitError] = useState(null);
+   const [isLoading, setisLoading] = useState(false);
+   // const [submitError, setSubmitError] = useState(null);
 
-  const [snackbarOpen, setSnackbarOpen] = useState(false);
-  const [message, setMessage] = useState(null);
-  const [severity, setSeverity] = useState("");
-  const [recaptcha, setRecaptcha] = useState(false);
+   const [snackbarOpen, setSnackbarOpen] = useState(false);
+   const [message, setMessage] = useState(null);
+   const [severity, setSeverity] = useState("");
+   const [recaptcha, setRecaptcha] = useState(false);
+   const [refreshReCaptcha, setRefreshReCaptcha] = useState(false);
 
-  const formik = useFormik({
-    initialValues: {
-      name: "",
-      email: "",
-    },
-    validationSchema: Yup.object({
-      name: Yup.string().required("Name is Required"),
-      email: Yup.string()
-        .email("Invalid Email Address")
-        .required("Email Address is Required"),
-    }),
-    onSubmit: (values) => {
-      alert(JSON.stringify(values, null, 2));
-    },
-  });
+   const formik = useFormik({
+      initialValues: {
+         name: "",
+         email: "",
+      },
+      validationSchema: Yup.object({
+         name: Yup.string().required("Name is Required"),
+         email: Yup.string()
+            .email("Invalid Email Address")
+            .required("Email Address is Required"),
+      }),
+      onSubmit: (values) => {
+         alert(JSON.stringify(values, null, 2));
+      },
+   });
 
-  const handleClose = (event, reason) => {
-    if (reason === "clickaway") {
-      return;
-    }
-
-    setSnackbarOpen(false);
-  };
-
-  const dataService = new DataService();
-
-  const handleSubscribe = async () => {
-    if (nameInfo.status && emailInfo.status) {
-      setisLoading(true);
-      const { status, error } = await dataService.handleSubscription({
-        name: name,
-        email: email,
-      });
-      if (status) {
-        setMessage("Subscribed Successfully");
-        setSeverity("success");
-        setSnackbarOpen(true);
-        setName("");
-        setEmail("");
-        setNameInfo({ error: null, status: false });
-        setEmailInfo({ error: null, status: false });
-      } else {
-        setMessage(error);
-        setSeverity("error");
-        setSnackbarOpen(true);
+   const handleClose = (event, reason) => {
+      if (reason === "clickaway") {
+         return;
       }
-      setisLoading(false);
-    }
-  };
 
-  function onChange(value) {
-    console.log("Captcha value:", value);
-    setRecaptcha(!recaptcha);
-  }
+      setSnackbarOpen(false);
+   };
 
-  return (
-    <Stack
-      direction={{ md: "row", sm: "column", xs: "column" }}
-      justifyContent="center"
-      alignItems="center"
-      spacing={1}
-    >
-      <SubscriptDiv fonts={fonts} md={7} sm={12}>
-        <SubIcon icon="entypo:mail" width="70" />
-        <Title fonts={fonts}>SUBSCRIBE US</Title>
-        <InfoLable>
-          Join our subscribe list to get the latest news & updates
-        </InfoLable>
-        <CssTextField
-          fullWidth
-          label="Name"
-          id="custom-css-outlined-input"
-          error={formik.touched.name && formik.errors.name}
-          helperText={formik.touched.name && formik.errors.name}
-          {...formik.getFieldProps("name")}
-        />
-        <CssTextField
-          fullWidth
-          label="Email Address"
-          id="custom-css-outlined-input"
-          error={formik.touched.email && formik.errors.email}
-          helperText={formik.touched.email && formik.errors.email}
-          {...formik.getFieldProps("email")}
-        />
-        <Box
-          sx={{
-            display: "flex",
-            justifyContent: "center",
-          }}
-        >
-          <RECAPTCHA onChange={onChange} />
-        </Box>
-        <SubBttn
-          // disabled={
-          //   isLoading || !nameInfo.status || !emailInfo.status || !recaptcha
-          // }
-          onClick={formik.handleSubmit}
-        >
-          SUBMIT
-        </SubBttn>
-        <CustomSnackBar
-          isOpen={snackbarOpen}
-          severity={severity}
-          handleClose={handleClose}
-          message={message}
-        />
-      </SubscriptDiv>
-      <Img src={SubscribeImg} alt="" />
-    </Stack>
-  );
+   const dataService = new DataService();
+
+   const handleSubscribe = async () => {
+      if (nameInfo.status && emailInfo.status) {
+         setisLoading(true);
+         const { status, error } = await dataService.handleSubscription({
+            name: name,
+            email: email,
+         });
+         if (status) {
+            setMessage("Subscribed Successfully");
+            setSeverity("success");
+            setSnackbarOpen(true);
+            setName("");
+            setEmail("");
+            setNameInfo({ error: null, status: false });
+            setEmailInfo({ error: null, status: false });
+         } else {
+            setMessage(error);
+            setSeverity("error");
+            setSnackbarOpen(true);
+         }
+         setisLoading(false);
+      }
+   };
+
+   function onChange(value) {
+      console.log("Captcha value:", value);
+      setRecaptcha(!recaptcha);
+   }
+
+   return (
+      <Stack
+         direction={{ md: "row", sm: "column", xs: "column" }}
+         justifyContent="center"
+         alignItems="center"
+         spacing={1}
+      >
+         <SubscriptDiv fonts={fonts} md={7} sm={12}>
+            <SubIcon icon="entypo:mail" width="70" />
+            <Title fonts={fonts}>SUBSCRIBE US</Title>
+            <InfoLable>
+               Join our subscribe list to get the latest news & updates
+            </InfoLable>
+            <CssTextField
+               fullWidth
+               label="Name"
+               id="custom-css-outlined-input"
+               error={formik.touched.name && formik.errors.name}
+               helperText={formik.touched.name && formik.errors.name}
+               {...formik.getFieldProps("name")}
+            />
+            <CssTextField
+               fullWidth
+               label="Email Address"
+               id="custom-css-outlined-input"
+               error={formik.touched.email && formik.errors.email}
+               helperText={formik.touched.email && formik.errors.email}
+               {...formik.getFieldProps("email")}
+            />
+            <Box
+               sx={{
+                  display: "flex",
+                  justifyContent: "center",
+               }}
+            >
+               <RECAPTCHA onChange={onChange} />
+            </Box>
+            <SubBttn
+               // disabled={
+               //   isLoading || !nameInfo.status || !emailInfo.status || !recaptcha
+               // }
+               onClick={formik.handleSubmit}
+            >
+               SUBMIT
+            </SubBttn>
+            <CustomSnackBar
+               isOpen={snackbarOpen}
+               severity={severity}
+               handleClose={handleClose}
+               message={message}
+            />
+         </SubscriptDiv>
+         <Img src={SubscribeImg} alt="" />
+      </Stack>
+   );
 }
 
 export default Subscription;
